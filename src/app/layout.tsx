74b06181d986d1f2c258447d44626ac0dc47adf1
Adeyemi Adejumo <adejumoadeyemi32@gmail.com>
2024-12-14 20:33:16 +0100

    :sparkles: added new project to the portfolio

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import "./font.css";
 
 export const metadata: Metadata = {
   title: "Adeyemi Adejumo",
@@ -12,11 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      suppressHydrationWarning
-      
-    >
+    <html lang="en" suppressHydrationWarning>
       <body className="bg-slate-100 dark:bg-slate-700">{children}</body>
     </html>
   );
